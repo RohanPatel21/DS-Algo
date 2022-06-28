2d62fdd355a402f1f91b2547b5f054afdf4b16d3
The-Insighter <rp3617@nyu.edu>
2022-06-29 02:20:59 +0530

    Initial commit

---
+++ LinkedLists/SinglyLinkedList.py
@@ -0,0 +1,18 @@
+class node:
+    def __init__(self , value = None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+     def __init__(self):
+        self.head = None
+        self.tail = None
+
+singlyLinkedList = SLinkedList()
+node1 = node(1)
+node2 = node(2)
+
+singlyLinkedList.head = node1
+singlyLinkedList.head.next = node2
+singlyLinkedList.tail = node2
+
