cec2e8fb63e1a5eaa7925e3f35a6067a7dd8be24
The-Insighter <rp3617@nyu.edu>
2022-06-29 03:40:02 +0530

    Initial commit

--- Arrays/Reverse list.py
+++ Arrays/Reverse list.py
@@ -2,7 +2,7 @@ def reverseList(A, start, end):
     while start<end:
         A[start],A[end] = A[end],A[start]
         end -=1
-        start+= 1
+        start += 1
 
 # Driver function to test above function
 A = [1, 2, 3, 4, 5, 6]
--- Arrays/Running Sum of 1d Array.py
+++ Arrays/Running Sum of 1d Array.py
@@ -1,8 +1,5 @@
-
-# def runningSum(self, nums):
-#     for i in range(1, len(nums)):
-#         nums[i] += nums[i-1]
-#         return nums
-nums = [1,2,3]
-for i in range(1, len(nums)):
-    print(i)
+class Solution(object):
+    def runningSum(self, nums):
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
