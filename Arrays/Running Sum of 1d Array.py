1f35de8542e50db6406da679a8b7206db8e18483
The-Insighter <rp3617@nyu.edu>
2022-06-29 02:20:59 +0530

    Initial commit

---
+++ Arrays/Running Sum of 1d Array.py
@@ -0,0 +1,8 @@
+
+# def runningSum(self, nums):
+#     for i in range(1, len(nums)):
+#         nums[i] += nums[i-1]
+#         return nums
+nums = [1,2,3]
+for i in range(1, len(nums)):
+    print(i)
