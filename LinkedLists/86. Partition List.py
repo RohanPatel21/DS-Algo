61f98ef8ce2ffd22fcad8ffbd5b1a6278db5b49c
The-Insighter <rp3617@nyu.edu>
2022-07-23 03:09:45 +0530

    -

---
+++ LinkedLists/86. Partition List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+
+        ltail.next = right.next
+        rtail.next = None
+        return left.next
