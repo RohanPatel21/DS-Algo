166a145f87dc1703e61f1d9cb18d665414fc9e55
The-Insighter <rp3617@nyu.edu>
2022-08-24 08:04:46 -0400

    -

--- rough.py
+++ rough.py
@@ -1,25 +1,11 @@
-# An example script to connect to Google using socket
-# programming in Python
-import socket # for socket
-import sys
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print("Socket successfully created")
-except socket.error as err:
-	print("socket creation failed with error %s" %(err))
-
-# default port for socket
-port = 80
-
-try:
-	host_ip = socket.gethostbyname('www.google.com')
-except socket.gaierror:
-
-	# this means could not resolve the host
-	print("there was an error resolving the host")
-	sys.exit()
-
-# connecting to the server
-s.connect((host_ip, port))
-
-print("the socket has successfully connected to google")
+string = 'shlok goswami'
+sub = 'SECRET'
+digits = ""
+if string != None and sub in string:
+	for i in string:
+		if i.isdigit():
+			digits = digits + i
+	print("Find numbers from string:", digits)
+	print(len(digits))
+else:
+    print("SECRET code not found")
