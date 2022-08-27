a1b0fcf7257d1269001a82bd505b4e1f31e339e8
RohanPatel21 <rp3617@nyu.edu>
2022-08-27 11:57:29 -0400

    -

---
+++ Hashmap/350. Intersection of Two Arrays II.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        c = Counter(nums1)
+        res = []
+        for n in nums2:
+            if c[n] > 0:
+                res.append(n)
+                c[n] -= 1
+        return res
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
