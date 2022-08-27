03391e7b2e57cf001adf6db002cf87c147d898c0
RohanPatel21 <rp3617@nyu.edu>
2022-08-27 11:19:21 -0400

    -

---
+++ Arrays/Binary Search/69. Sqrt(x).py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mySqrt(self, x):
+        l = 0
+        r = x
+        while l + 1 < r:
+            m = (l + r) / 2
+            if m * m > x: r = m
+            if m * m < x: l = m
+            if m * m == x: return int(m)
+        if r * r == x: return int(r)
+        return ((l + r) / 2)
+
+        """
+        :type x: int
+        :rtype: int
+        """
