b7f3d30968cdf68e4cb5ab1df4c28181805f8f89
RohanPatel21 <rp3617@nyu.edu>
2022-08-27 14:11:55 -0400

    -

---
+++ Arrays/Binary Search/240. Search a 2D Matrix II.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        height = len(matrix)
+        width = len(matrix[0])
+        row = height - 1
+        col = 0
+        while col < width and row >= 0:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else:  # found it
+                return True
+
+        return False
+
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
